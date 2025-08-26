4b9eda4fc51622761b60375db71ea77786f2d7e6
MhassaanY <mdhassaany@gmail.com>
2025-08-26 19:11:31 +0500

    feat: Make database URL configurable via environment variable

--- matrisks-backend/app/database.py
+++ matrisks-backend/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost/matrisks")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/matrisks")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
